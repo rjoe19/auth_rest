92689d8fa95fb3d53b86151203a0437148af6bd6
Richard Joe <rjoe19@hotmail.com>
2016-08-25 18:19:11 +1200

    setup passport.js

---
+++ config/passport.js
@@ -0,0 +1,22 @@
+var jwtStrategy = require('passport-jwt').Strategy;
+
+var User = require('../app/models/user.js');
+var config = require('../config/database');
+
+module.exports = function(passport) {
+  var opts = {};
+  opts.secretOrKey = config.secret;
+  passport.use(new jwtStrategy(opts, function(jwt_payload, done){
+    User.find({id:jwt_payload.id}, function(err, user) {
+      if(err) {
+        return done(err, false;)
+      }
+      if(user) {
+        done(null, user);
+      }
+      else {
+        done(null, false);
+      }
+    })
+  }))
+};
